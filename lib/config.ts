cebe03c5029d5951c23d0764a2cb4100375f8c52
Dariusz Piwko <littlejohn19@users.noreply.github.com>
2023-12-08 12:25:53 +0100

    Update config.ts

--- lib/config.ts
+++ lib/config.ts
@@ -1,7 +1,3 @@
 export const config = {
-    port: process.env.PORT || 3100,
-    // url: (network: string) => {
-    //     const userNetwork = network.split('-')[1] || network;
-    //     return `http://api.adp.onetapi.pl/${userNetwork}/v1/`
-    // }
+    port: process.env.PORT || 3100
 };
